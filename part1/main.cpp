fdfa214f460bae73b108d30337bad92547e237b7
Jeremy Eng <jeremy@jeremyeng.com>
2020-01-21 17:10:40 -0500

    add get_type function

--- part1/main.cpp
+++ part1/main.cpp
@@ -4,6 +4,43 @@
 #include "list.h"
 #include <cstring>
 #include <unistd.h>
+#include <regex.h>
+
+Column::data_type get_type(char* field) {
+  regex_t string_regex;
+  regex_t float_regex;
+  regex_t bool_regex;
+  regex_t int_regex;
+
+  int string_regex_compiled = regcomp(&string_regex, "[a-zA-Z]+", REG_EXTENDED);
+  int float_regex_compiled = regcomp(&float_regex, "[-+]?[0-9]*\\.+[0-9]+", REG_EXTENDED);
+  int bool_regex_compiled = regcomp(&bool_regex, "^[0|1]$", REG_EXTENDED);
+  int int_regex_compiled = regcomp(&int_regex, "[-+]?[0-9]+", REG_EXTENDED);
+
+  assert(string_regex_compiled==0);
+  assert(float_regex_compiled==0);
+  assert(bool_regex_compiled==0);
+  assert(int_regex_compiled==0);
+
+  Column::data_type field_type;
+  if(regexec(&string_regex, field, 0, NULL, 0) == 0) {
+    field_type = Column::STRING;
+  } else if(regexec(&float_regex, field, 0, NULL, 0) == 0) {
+    field_type = Column::FLOAT;
+  } else if(regexec(&bool_regex, field, 0, NULL, 0) == 0) {
+    field_type = Column::BOOL;
+  } else {
+    field_type = Column::INT;
+  }
+
+  // free regexes
+  regfree(&float_regex);
+  regfree(&string_regex);
+  regfree(&int_regex);
+  regfree(&bool_regex);
+
+  return field_type;
+};
 
 int main(int argc, char** argv) {
 
